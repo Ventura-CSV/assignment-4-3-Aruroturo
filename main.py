7801c7e165638c8ebe23ca88177e693236162694
Aruroturo <arturo.alfaro3@my.vcccd.edu>
2024-06-28 11:52:45 -0700

    append

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ def main():
     Code Your Program here
     ########################################
     """
-
+    while number > 0:
+        Camerino = number % 2
+        result.append(Camerino)
+        number = number // 2
+    result.reverse()
     print(*result)
     ########################################
     # Do not delete the return statement
