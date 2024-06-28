c9c6c29e3189d0be94f6253500bd5ab1bc1e55f9
Aruroturo <arturo.alfaro3@my.vcccd.edu>
2024-06-28 11:55:26 -0700

    try again

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
         Camerino = number % 2
         result.append(Camerino)
         number = number // 2
-    result.reverse()
+    result
     print(*result)
     ########################################
     # Do not delete the return statement
